d1c3e37599f7e1e70cebe0da975ce8c11f887e26
Aaron Robinson <aaronrobin1234@gmail.com>
2023-12-01 00:54:58 -0600

    New day 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -8,37 +8,38 @@ part1 :: [String] -> Int
 part1 ls = sum (read . firstLastDigit <$> ls)
 
 firstLastNum :: String -> String
-firstLastNum s = 
-  case foldl findFirstAndLast (Nothing, Nothing) $ droppingHead s of
+firstLastNum line = 
+  case foldl findFirstAndLast (Nothing, Nothing) $ droppingHead line of
     (Just f, Just l) -> [f, l]
-    _ -> error s
+    _ -> error line
   where
     droppingHead :: [a] -> [[a]]
     droppingHead [] = []
     droppingHead xs@(_:xs') = xs : droppingHead xs'
 
+    startsWith :: Eq e => [e] -> [e] -> Bool
+    startsWith [] _ = True
+    startsWith (_:_) [] = False
+    startsWith (x:xs) (y:ys) = x == y && startsWith xs ys
+
+    replaceIfStartsWithAny :: (Maybe Char, Maybe Char) -> Char -> [String] -> String -> (Maybe Char, Maybe Char)
+    replaceIfStartsWithAny fl c choices s = if any (`startsWith` s) choices then replaceNeeded fl c else fl
+
     findFirstAndLast :: (Maybe Char, Maybe Char) -> String -> (Maybe Char, Maybe Char)
     findFirstAndLast firstAndLast nextWindow =
-      case nextWindow of
-        '1' : _ -> replaceNeeded firstAndLast '1'
-        'o' : 'n' : 'e' : _ -> replaceNeeded firstAndLast '1'
-        '2' : _ -> replaceNeeded firstAndLast '2'
-        't' : 'w' : 'o' : _ -> replaceNeeded firstAndLast '2'
-        '3' : _ -> replaceNeeded firstAndLast '3'
-        't' : 'h' : 'r' : 'e' : 'e' : _ -> replaceNeeded firstAndLast '3'
-        '4' : _ -> replaceNeeded firstAndLast '4'
-        'f' : 'o' : 'u' : 'r' : _ -> replaceNeeded firstAndLast '4'
-        '5' : _ -> replaceNeeded firstAndLast '5'
-        'f' : 'i' : 'v' : 'e' : _ -> replaceNeeded firstAndLast '5'
-        '6' : _ -> replaceNeeded firstAndLast '6'
-        's' : 'i' : 'x' : _ -> replaceNeeded firstAndLast '6'
-        '7' : _ -> replaceNeeded firstAndLast '7'
-        's' : 'e' : 'v' : 'e' : 'n' : _ -> replaceNeeded firstAndLast '7'
-        '8' : _ -> replaceNeeded firstAndLast '8'
-        'e' : 'i' : 'g' : 'h' : 't' : _ -> replaceNeeded firstAndLast '8'
-        '9' : _ -> replaceNeeded firstAndLast '9'
-        'n' : 'i' : 'n' : 'e' : _ -> replaceNeeded firstAndLast '9'
-        _ -> firstAndLast
+      let pairs = 
+            [
+              ('1', ["1", "one"]),
+              ('2', ["2", "two"]),
+              ('3', ["3", "three"]),
+              ('4', ["4", "four"]),
+              ('5', ["5", "five"]),
+              ('6', ["6", "six"]),
+              ('7', ["7", "seven"]),
+              ('8', ["8", "eight"]),
+              ('9', ["9", "nine"])
+            ]
+      in foldr (\(c, choices) fl -> replaceIfStartsWithAny fl c choices nextWindow) firstAndLast pairs
 
     replaceNeeded (Nothing, _) new = (Just new, Just new)
     replaceNeeded (Just f, _) new = (Just f, Just new)
