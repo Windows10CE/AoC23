50559c9881b2ad8cdc0029c17557114c0edf10f9
Aaron Robinson <aaronrobin1234@gmail.com>
2023-12-02 00:36:02 -0600

    Fixup day 1, add benchmarking

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,7 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Main (main) where
+
+import Criterion.Main
 
 import System.Environment (getArgs)
 import qualified Day01 as D1
@@ -12,11 +15,21 @@ solvers =
   ]
 
 main :: IO ()
-main = do
+main = doRun
+
+doRun :: IO ()
+doRun = do
   args <- getArgs
   let day = read (head args)
   (part1, part2) <- solveDay (solvers !! (day - 1)) day
   putStrLn $ "Part 1: " ++ part1 ++ "\nPart 2: " ++ part2
+
+doBench :: IO ()
+doBench = do
+  days <- mapM (\(s, i) -> (s, i,) <$> readFile ("inputs/" ++ show i)) (zip solvers ([1..] :: [Int]))
+  defaultMain [
+      bgroup "Days" $ (\(solver, day, input) -> bench (show day) $ nf solver input) <$> days
+    ]
 
 solveDay :: (String -> (String, String)) -> Int -> IO (String, String)
 solveDay solver day = do
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,5 +1,6 @@
 module Day01 (solve) where
 import Data.Char (isDigit)
+import Data.List (isPrefixOf, tails)
 
 firstLastDigit :: String -> String
 firstLastDigit s = let digits = filter isDigit s in [head digits, last digits]
@@ -7,37 +8,28 @@ firstLastDigit s = let digits = filter isDigit s in [head digits, last digits]
 part1 :: [String] -> Int
 part1 ls = sum (read . firstLastDigit <$> ls)
 
-firstLastNum :: String -> String
+firstLastNum :: String -> Int
 firstLastNum line = 
-  case foldl findFirstAndLast (Nothing, Nothing) $ droppingHead line of
-    (Just f, Just l) -> [f, l]
+  case foldl findFirstAndLast (Nothing, Nothing) $ tails line of
+    (Just f, Just l) -> f * 10 + l
     _ -> error line
   where
-    droppingHead :: [a] -> [[a]]
-    droppingHead [] = []
-    droppingHead xs@(_:xs') = xs : droppingHead xs'
+    replaceIfStartsWithAny :: (Maybe Int, Maybe Int) -> Int -> [String] -> String -> (Maybe Int, Maybe Int)
+    replaceIfStartsWithAny fl c choices s = if any (`isPrefixOf` s) choices then replaceNeeded fl c else fl
 
-    startsWith :: Eq e => [e] -> [e] -> Bool
-    startsWith [] _ = True
-    startsWith (_:_) [] = False
-    startsWith (x:xs) (y:ys) = x == y && startsWith xs ys
-
-    replaceIfStartsWithAny :: (Maybe Char, Maybe Char) -> Char -> [String] -> String -> (Maybe Char, Maybe Char)
-    replaceIfStartsWithAny fl c choices s = if any (`startsWith` s) choices then replaceNeeded fl c else fl
-
-    findFirstAndLast :: (Maybe Char, Maybe Char) -> String -> (Maybe Char, Maybe Char)
+    findFirstAndLast :: (Maybe Int, Maybe Int) -> String -> (Maybe Int, Maybe Int)
     findFirstAndLast firstAndLast nextWindow =
       let pairs = 
             [
-              ('1', ["1", "one"]),
-              ('2', ["2", "two"]),
-              ('3', ["3", "three"]),
-              ('4', ["4", "four"]),
-              ('5', ["5", "five"]),
-              ('6', ["6", "six"]),
-              ('7', ["7", "seven"]),
-              ('8', ["8", "eight"]),
-              ('9', ["9", "nine"])
+              (1, ["1", "one"]),
+              (2, ["2", "two"]),
+              (3, ["3", "three"]),
+              (4, ["4", "four"]),
+              (5, ["5", "five"]),
+              (6, ["6", "six"]),
+              (7, ["7", "seven"]),
+              (8, ["8", "eight"]),
+              (9, ["9", "nine"])
             ]
       in foldr (\(c, choices) fl -> replaceIfStartsWithAny fl c choices nextWindow) firstAndLast pairs
 
@@ -45,7 +37,7 @@ firstLastNum line =
     replaceNeeded (Just f, _) new = (Just f, Just new)
 
 part2 :: [String] -> Int
-part2 ls = sum (read . firstLastNum <$> ls)
+part2 ls = sum (firstLastNum <$> ls)
 
 solve :: String -> (String, String)
 solve input =
